59710c9b2bc51ee70e43c3ce5cdc521a6681229a
Deekshika Sharma <deekshika493@gmail.com>
2026-06-07 19:46:37 +0530

    Add majorityElement method to Solution class

--- dict.py
+++ dict.py
@@ -23,3 +23,12 @@ for ch in s:
     freq[ch] = freq.get(ch,0)+1
 print(freq)
 
+***********************************************************************
+#majority element  
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] > len(nums) // 2:
+                return num
