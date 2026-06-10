db26194b87f53c4294751240374a3b50d9cc4741
Deekshika Sharma <deekshika493@gmail.com>
2026-06-10 19:27:30 +0530

    Add recursive and iterative Fibonacci functions

--- learning-python.py
+++ learning-python.py
@@ -40,3 +40,25 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.strip().split()[-1])
         #ladt word count
+
+************************************************************************
+#recursive
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
+
+
+        #iterative 
+        class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+
+        a, b = 0, 1
+
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+
+        return b
