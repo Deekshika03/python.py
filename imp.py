9779d80cf18164f7f13ed481921e0dbb9f9830fe
Deekshika Sharma <deekshika493@gmail.com>
2026-07-12 21:58:53 +0530

    Add minCostClimbingStairs method to Solution class
    Implement minCostClimbingStairs method to calculate minimum cost.

--- imp.py
+++ imp.py
@@ -46,3 +46,15 @@ def valid(s):
 
     return not stack
 
+#leetcode problem : minimum climbing stairs
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        prev2 = cost[0]
+        prev1 = cost[1]
+
+        for i in range(2, len(cost)):
+            curr = cost[i] + min(prev1, prev2)
+            prev2 = prev1
+            prev1 = curr
+
+        return min(prev1, prev2)
